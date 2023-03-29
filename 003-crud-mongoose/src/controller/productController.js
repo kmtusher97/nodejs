25b4c66e1a0fe3273eb32cadfb49b31a89777ff3
kmtusher97 <kmtusher97@gmail.com>
2023-03-29 12:07:04 +0600

    move validation to model complete create product api

--- 003-crud-mongoose/src/controller/productController.js
+++ 003-crud-mongoose/src/controller/productController.js
@@ -1,19 +1,11 @@
 const ProductService = require('../service/productService');
 
 const createProduct = async (req, res) => {
-  const productPayload = req.body;
-
-  // validation
-  if (!productPayload.name || !productPayload.price) {
-    res.status(400).json('invalid payload');
-    return;
-  }
-
   try {
     const product = await ProductService.createProduct({
-      productPayload,
+      productPayload: req.body,
     });
-    res.status(201).json();
+    res.status(201).json(product);
   } catch (error) {
     res.status(400).json(error.message);
   }
--- 003-crud-mongoose/src/model/product.js
+++ 003-crud-mongoose/src/model/product.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
+  name: {
+    type: String,
+    required: [true, 'name is required'],
+    maxLength: [10, 'name should contain at max 10 letters'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'price is required'],
+    min: [0, 'price cannot be negative'],
+    max: [100000, 'price cannot be greater than 10000'],
+  },
   quantity: { type: Number, default: 0 },
   unit: { type: String },
 });
